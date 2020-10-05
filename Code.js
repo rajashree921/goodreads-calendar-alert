5e6ffe702cffe52b214d28493e3287be6f9d292f
rajashree921 <rajashree921@gmail.com>
2020-10-06 01:38:18 +0530

    Updated missing info to be stored in Sheets

--- Code.js
+++ Code.js
@@ -17,7 +17,9 @@ var results = [],
     method: "GET",
     payload: payload,
     muteHttpExceptions: true,
-  };
+  },
+  ss = SpreadsheetApp.openById("1dXbvJc9i1KoiMUaTNukCYX9DS8oGmyznnXsZe4-BFZM"),
+  sheet = ss.getSheets()[0];
 
 function getBooks(firstWeekOfMonth) {
   var response = UrlFetchApp.fetch(apiUrl, params);
@@ -31,43 +33,52 @@ function getBooks(firstWeekOfMonth) {
 
     results.getChildren().forEach(function (result) {
       result.getChildren("book").forEach(function (book) {
-        var titleWithoutSeries = book.getChild("title_without_series").getText();
-        if (calendarEventNotExists(titleWithoutSeries)){ //check whether event already exists for the book
-          if (book.getChild("publication_day").getText()) { //check whether publication date values are filled in
+        var title = book.getChild("title").getText(),
+          titleWithoutSeries = book.getChild("title_without_series").getText(),
+          author = book
+            .getChild("authors")
+            .getChild("author")
+            .getChild("name")
+            .getText(),
+          link = book.getChild("link").getText();
+        if (calendarEventNotExists(titleWithoutSeries)) {
+          //check whether event already exists for the book
+          if (book.getChild("publication_day").getText()) {
+            //check whether publication date values are filled in
             var pubDate = new Date(
               book.getChild("publication_year").getText(),
               book.getChild("publication_month").getText() - 1,
               book.getChild("publication_day").getText()
             );
-            if (pubDate >= today) { //check whether book has a future release date
-              var author = book
-                  .getChild("authors")
-                  .getChild("author")
-                  .getChild("name")
-                  .getText(),
-                title = book.getChild("title").getText(),
-                link = book.getChild("link").getText();
-              
-                pubDate.setDate(pubDate.getDate() + 2);
-                title = title.concat(" - ", author);
-                CalendarApp.getDefaultCalendar().createAllDayEvent(
-                  title,
-                  pubDate,
-                  { location: link }
-                );
-                if (debug)
-                  Logger.log("\nEvent created: " + title + "\nDate: " + pubDate);
+            if (pubDate >= today) {
+              //check whether book has a future release date
+              pubDate.setDate(pubDate.getDate() + 2);
+              title = title.concat(" - ", author);
+              CalendarApp.getDefaultCalendar().createAllDayEvent(
+                title,
+                pubDate,
+                { location: link }
+              );
+              if (debug)
+                Logger.log("\nEvent created: " + title + "\nDate: " + pubDate);
+              textFinder = ss.createTextFinder(titleWithoutSeries);
+              position = textFinder.findNext();
+              if (position != null) {
+                sheet.deleteRow(position.getRow());
+              }
             }
-          } 
-          else if (firstWeekOfMonth) {
-          //if publication date is missing and it's first week of the month/running in debug mode, add the book link to the list.
-            missingInfo.push(book.getChild("link").getText());
+          } else if (firstWeekOfMonth) {
+            //if publication date is missing and it's first week of the month/running in debug mode, add the book link to the sheet.
+            textFinder = ss.createTextFinder(title);
+            if (textFinder.findNext() == null) {
+              sheet.appendRow([title, author, link]);
+            }
           }
         }
       });
     });
   } else {
-    Logger.log("error");
+    Logger.log("API request error");
   }
 }
 
@@ -99,14 +110,5 @@ function main() {
     payload.sort = "date_pub";
     payload.per_page = 100;
     getBooks(true);
-  }
-  if (missingInfo.length) {
-    var recipient = Session.getActiveUser().getEmail(),
-      subject = "GR_cal: Books with missing dates",
-      body = "";
-    for (i = 0; i < missingInfo.length; i++) {
-      body += missingInfo[i] + "\n";
-    }
-    MailApp.sendEmail(recipient, subject, body);
   }
 }
